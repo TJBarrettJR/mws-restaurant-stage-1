dc45b771123583bfd2cd842b8126a7ed51a06dbd
tjbarre1 <tjbarre1@asu.edu>
2018-09-13 23:49:23 -0400

    FEAT: Removed google maps and changed to mapbox meets performance and accessibility scores

--- js/main.js
+++ js/main.js
@@ -20,6 +20,7 @@ if('serviceWorker' in navigator) { // found from https://developer.mozilla.org/e
  * Fetch neighborhoods and cuisines as soon as the page is loaded.
  */
 document.addEventListener('DOMContentLoaded', (event) => {
+  initMap();
   fetchNeighborhoods();
   fetchCuisines();
 });
@@ -80,8 +81,29 @@ fillCuisinesHTML = (cuisines = self.cuisines) => {
 }
 
 /**
- * Initialize Google map, called from HTML.
+ * Initialize leaflet map, called from HTML.
  */
+initMap = () => {
+  self.newMap = L.map('map', {
+        center: [40.722216, -73.987501],
+        zoom: 12,
+        scrollWheelZoom: false
+      });
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
+    mapboxToken: 'pk.eyJ1IjoidGpiYXJyZXR0IiwiYSI6ImNqbTFnZ2tobTE0aTczcHJ2NG9ndDlnaGsifQ.-bAXZ3VwoZnrFmU8XkgNRw',
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox.streets'
+  }).addTo(newMap);
+
+  updateRestaurants();
+}
+
+/**
+ * Initialize Google map, called from HTML.
+ 
 window.initMap = () => {
   let loc = {
     lat: 40.722216,
@@ -94,6 +116,7 @@ window.initMap = () => {
   });
   updateRestaurants();
 }
+*/
 
 /**
  * Update page and map for current restaurants.
@@ -128,7 +151,9 @@ resetRestaurants = (restaurants) => {
   ul.innerHTML = '';
 
   // Remove all map markers
-  self.markers.forEach(m => m.setMap(null));
+  if (self.markers) {
+    self.markers.forEach(m => marker.remove());
+  }
   self.markers = [];
   self.restaurants = restaurants;
 }
@@ -179,7 +204,24 @@ createRestaurantHTML = (restaurant) => {
 /**
  * Add markers for current restaurants to the map.
  */
+
+/**
+ * Add markers for current restaurants to the map.
+ */
 addMarkersToMap = (restaurants = self.restaurants) => {
+  restaurants.forEach(restaurant => {
+    // Add marker to the map
+    const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.newMap);
+    marker.on("click", onClick);
+    function onClick() {
+      window.location.href = marker.options.url;
+    }
+    self.markers.push(marker);
+  });
+
+} 
+
+/*addMarkersToMap = (restaurants = self.restaurants) => {
   restaurants.forEach(restaurant => {
     // Add marker to the map
     const marker = DBHelper.mapMarkerForRestaurant(restaurant, self.map);
@@ -188,4 +230,4 @@ addMarkersToMap = (restaurants = self.restaurants) => {
     });
     self.markers.push(marker);
   });
-}
+}*/

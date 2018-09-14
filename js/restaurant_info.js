3d35d682a3de4ee9d14bab1912f887e819dfe417
tjbarre1 <tjbarre1@asu.edu>
2018-09-14 00:11:21 -0400

    BUGFIX: Added changes to restaurant_info.js for mapbox changes

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -1,10 +1,10 @@
 let restaurant;
-var map;
+var newMap;
 
 /**
  * Initialize Google map, called from HTML.
  */
-window.initMap = () => {
+/*window.initMap = () => {
   fetchRestaurantFromURL((error, restaurant) => {
     if (error) { // Got an error!
       console.error(error);
@@ -18,7 +18,41 @@ window.initMap = () => {
       DBHelper.mapMarkerForRestaurant(self.restaurant, self.map);
     }
   });
-}
+}*/
+
+/**
+ * Initialize map as soon as the page is loaded.
+ */
+document.addEventListener('DOMContentLoaded', (event) => {  
+  initMap();
+});
+
+/**
+ * Initialize leaflet map
+ */
+initMap = () => {
+  fetchRestaurantFromURL((error, restaurant) => {
+    if (error) { // Got an error!
+      console.error(error);
+    } else {      
+      self.newMap = L.map('map', {
+        center: [restaurant.latlng.lat, restaurant.latlng.lng],
+        zoom: 16,
+        scrollWheelZoom: false
+      });
+      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
+        mapboxToken: 'pk.eyJ1IjoidGpiYXJyZXR0IiwiYSI6ImNqbTFnZ2tobTE0aTczcHJ2NG9ndDlnaGsifQ.-bAXZ3VwoZnrFmU8XkgNRw',
+        maxZoom: 18,
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+          '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+          'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        id: 'mapbox.streets'    
+      }).addTo(newMap);
+      fillBreadcrumb();
+      DBHelper.mapMarkerForRestaurant(self.restaurant, self.newMap);
+    }
+  });
+}  
 
 /**
  * Get current restaurant from page URL.
